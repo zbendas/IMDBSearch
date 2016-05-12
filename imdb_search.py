63d766cfc956690532dec5b7d27d8e5df14321be
Zachariah Bendas <zbendas@yahoo.com>
2016-05-12 11:14:03 -0500

    Revert "Rewrote imdb_search to incorporate ratings, as well as diminish number of requests calls. Implemented optional printing. Added test_case_2.py to test new features."
    This reverts commit bab197db5fb31c576c9be090fce8dbfad56f4382.

--- imdb_search.py
+++ imdb_search.py
@@ -23,7 +23,7 @@ def get_search_results(override=""):
         # print "Search result URL: ", search_result[0] #Debug statement
 
 
-def get_html_tree(movie_link):
+def show_summary(movie_link):
     # Determine if passed value is False (which means null value in previous function)
     if movie_link is False:
         return False
@@ -31,10 +31,6 @@ def get_html_tree(movie_link):
     movie_page = requests.get('http://www.imdb.com' + str(movie_link))
     # Generate an HTML tree of movie page
     movie_tree = html.fromstring(movie_page.content)
-    return movie_tree
-
-
-def show_summary(movie_tree):
     # Grab the summary (returns as list)
     xpath = movie_tree.xpath('//*[@id="title-overview-widget"]//div[@class="summary_text"]/text()|'
                              '//*[@id="title-overview-widget"]//div[@class="summary_text"]/a/text()')
@@ -43,30 +39,14 @@ def show_summary(movie_tree):
     summary = summary.strip()
     # Return the string summary or error handler if no summary found
     if summary:
-        print("Summary:", summary)
+        print("Summary:", summary, "\n")
     else:
         print("Summary unavailable for this title.")
         return False
 
 
-def show_rating(movie_tree):
-    # Grab the rating (returns as list)
-    xpath = movie_tree.xpath('//*[@id="title-overview-widget"]//div[@class="imdbRating"]//strong/@title')
-    rating = ''.join(xpath)
-    rating = rating.strip()
-    if rating:
-        print(rating, ".", sep='')
-    else:
-        print("Title is unrated.")
-
-
-def search_imdb(override="", print_summary=True, print_rating=True):
+def search_imdb(override=""):
     # Run search, save results into local variable
     search_string = get_search_results(override)
-    # Pass resulting search URL into get_html_tree
-    movie_tree = get_html_tree(search_string)
-    if print_summary:
-        show_summary(movie_tree)
-    if print_rating:
-        show_rating(movie_tree)
-    print('\n', end='')
+    # Pass resulting search URL into ShowSummary
+    show_summary(search_string)
