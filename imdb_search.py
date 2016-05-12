ff793aad6bacf9677c9adcfe7d886bd847132f04
Zachariah Bendas <zbendas@yahoo.com>
2016-05-12 11:16:36 -0500

    Revert "Revert "Rewrote imdb_search to incorporate ratings, as well as diminish number of requests calls. Implemented optional printing. Added test_case_2.py to test new features.""
    This reverts commit 63d766cfc956690532dec5b7d27d8e5df14321be.

--- imdb_search.py
+++ imdb_search.py
@@ -23,7 +23,7 @@ def get_search_results(override=""):
         # print "Search result URL: ", search_result[0] #Debug statement
 
 
-def show_summary(movie_link):
+def get_html_tree(movie_link):
     # Determine if passed value is False (which means null value in previous function)
     if movie_link is False:
         return False
@@ -31,6 +31,10 @@ def show_summary(movie_link):
     movie_page = requests.get('http://www.imdb.com' + str(movie_link))
     # Generate an HTML tree of movie page
     movie_tree = html.fromstring(movie_page.content)
+    return movie_tree
+
+
+def show_summary(movie_tree):
     # Grab the summary (returns as list)
     xpath = movie_tree.xpath('//*[@id="title-overview-widget"]//div[@class="summary_text"]/text()|'
                              '//*[@id="title-overview-widget"]//div[@class="summary_text"]/a/text()')
@@ -39,14 +43,30 @@ def show_summary(movie_link):
     summary = summary.strip()
     # Return the string summary or error handler if no summary found
     if summary:
-        print("Summary:", summary, "\n")
+        print("Summary:", summary)
     else:
         print("Summary unavailable for this title.")
         return False
 
 
-def search_imdb(override=""):
+def show_rating(movie_tree):
+    # Grab the rating (returns as list)
+    xpath = movie_tree.xpath('//*[@id="title-overview-widget"]//div[@class="imdbRating"]//strong/@title')
+    rating = ''.join(xpath)
+    rating = rating.strip()
+    if rating:
+        print(rating, ".", sep='')
+    else:
+        print("Title is unrated.")
+
+
+def search_imdb(override="", print_summary=True, print_rating=True):
     # Run search, save results into local variable
     search_string = get_search_results(override)
-    # Pass resulting search URL into ShowSummary
-    show_summary(search_string)
+    # Pass resulting search URL into get_html_tree
+    movie_tree = get_html_tree(search_string)
+    if print_summary:
+        show_summary(movie_tree)
+    if print_rating:
+        show_rating(movie_tree)
+    print('\n', end='')
---
+++ test_case_2.py
@@ -0,0 +1,5 @@
+import imdb_search
+
+imdb_search.search_imdb("The Aviator")
+imdb_search.search_imdb("Mommie Dearest", print_rating=False)
+imdb_search.search_imdb("Avatar 3", print_summary=False)
