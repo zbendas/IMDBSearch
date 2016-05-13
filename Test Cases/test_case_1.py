ad4b682aaf9b2b256a77ac5f9f13eef1feb48432
Zachariah Bendas <zbendas@yahoo.com>
2016-05-13 11:59:44 -0500

    Moved Test Cases to their own folder

---
+++ Test Cases/test_case_1.py
@@ -0,0 +1,4 @@
+import imdb_search
+
+imdb_search.search_imdb("The Aviator")
+imdb_search.search_imdb("Mommie Dearest")
---
+++ Test Cases/test_case_4.py
@@ -0,0 +1,10 @@
+import imdb_search
+
+# Standard tests taken from others
+imdb_search.search_imdb("The Aviator")
+imdb_search.search_imdb("Avatar 3")
+
+# Following tests are attempting to force the "[x] not found" debug statements to print
+print(imdb_search.misc_format(["PG-13", "runtime"],[],[]))
+print(imdb_search.misc_format([],["2h 50min", "genre"],[]))
+print(imdb_search.misc_format([],[],["Biography", "Horror", "rating"]))
